f85ca6e972e2e399e1a008fef8c2850deca1ea50
aignarong <andrew.vathanakamsang@gmail.com>
2015-09-29 20:41:04 -0700

    updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,8 @@
 $( document ).ready(function() {
 	$( "clickedthisone" ).click(function() {
   		console.log("clicked!")
+  		$("#clickedthisone").removeClass("blue")
+  		$"#clickedthisone".addClass"red")
+
 	});
 });
